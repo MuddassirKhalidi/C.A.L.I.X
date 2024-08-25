26797bd8654e01e88f0500dbf0bfececb55d3949
Muddassir Khalidi <muddassirnawazkhan@gmail.com>
2024-08-25 17:59:55 +0300

    Renamed AIDA to ALARA

---
+++ alara/alara-app/lib/home_page.dart
@@ -0,0 +1,212 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<String> _sentences;
+  late PageController _pageController;
+  final TextEditingController _nameController = TextEditingController();
+  String? _storedName;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _sentences = [
+      "Welcome to ALARA!",
+      "Your personal voice assistant",
+      "Record. Recall.",
+      "Never miss a moment!"
+    ];
+
+    _pageController = PageController(
+      initialPage: _sentences.length * 1000,
+    );
+
+    _loadName();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      _startSentenceSwitch();
+    });
+  }
+
+  void _startSentenceSwitch() {
+    Timer.periodic(const Duration(seconds: 5), (timer) {
+      int currentPage = _pageController.page!.toInt();
+      _pageController.animateToPage(
+        currentPage + 1,
+        duration: const Duration(seconds: 1),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  Future<void> _loadName() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _storedName = prefs.getString('name') ?? '';
+
+      // Capitalize the first letter of the stored name
+      if (_storedName!.isNotEmpty) {
+        _storedName = _storedName![0].toUpperCase() + _storedName!.substring(1);
+      }
+
+      _nameController.text = _storedName!;
+
+      if (_storedName!.isNotEmpty) {
+        _sentences.insert(1, "Welcome, $_storedName!");
+      }
+    });
+  }
+
+  Future<void> _saveName() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('name', _nameController.text);
+    setState(() {
+      _storedName = _nameController.text;
+
+      if (_storedName!.isNotEmpty) {
+        if (_sentences.contains("Welcome, $_storedName!")) {
+          int index = _sentences.indexOf("Welcome, $_storedName!");
+          _sentences[index] = "Welcome, $_storedName!";
+        } else {
+          _sentences.insert(1, "Welcome, $_storedName!");
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // Background Image
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/IMG_5469.JPG'),
+                fit: BoxFit.cover,
+                alignment: Alignment.center),
+          ),
+        ),
+        // Foreground content
+        Scaffold(
+          backgroundColor: Colors.transparent, // Make the Scaffold transparent
+          body: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0, vertical: 20.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 65,
+                    ),
+                    TextField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Enter your name',
+                        labelStyle: GoogleFonts.robotoMono(
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white24,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                      style: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _saveName,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(159, 51, 2, 82),
+                      ),
+                      child: Text(
+                        'Save Name',
+                        style: GoogleFonts.robotoMono(
+                          textStyle: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Stack(
+                  children: [
+                    PageView.builder(
+                      controller: _pageController,
+                      itemCount: null,
+                      itemBuilder: (context, index) {
+                        int sentenceIndex = index % _sentences.length;
+                        return Center(
+                          child: Text(
+                            _sentences[sentenceIndex],
+                            style: GoogleFonts.robotoMono(
+                              textStyle: const TextStyle(
+                                fontSize: 27,
+                                color: Color(0xFFCBC5D0),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        );
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 20.0),
+                        child: SmoothPageIndicator(
+                          controller: _pageController,
+                          count: _sentences.length,
+                          effect: const WormEffect(
+                            activeDotColor: Colors.white,
+                            dotColor: Colors.grey,
+                            dotHeight: 8.0,
+                            dotWidth: 8.0,
+                            spacing: 16.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40.0),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ alara/alara-app/lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:motion_tab_bar/MotionTabBar.dart';
+import 'home_page.dart';
+import 'recording_page.dart';
+import 'asking_page.dart';
+
+void main() {
+  runApp(const ALARA());
+}
+
+class ALARA extends StatelessWidget {
+  const ALARA({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ALARA',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  bool _isRecording = false;
+  bool _isAsking = false;
+
+  late List<Widget> _pages;
+
+  @override
+  void initState() {
+    super.initState();
+    _pages = [
+      const HomePage(),
+      RecordingPage(
+        onRecordingStateChanged: (isRecording) {
+          setState(() {
+            _isRecording = isRecording;
+          });
+        },
+      ),
+      AskingPage(
+        onAskingStateChanged: (isAsking) {
+          setState(() {
+            _isAsking = isAsking;
+          });
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: GestureDetector(
+        onTap: () {
+          if (_isRecording || _isAsking) {
+            // Show a message or perform any action if needed
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'Please stop recording or asking before navigating.',
+                  style: GoogleFonts.robotoMono(),
+                ),
+              ),
+            );
+          }
+        },
+        child: AbsorbPointer(
+          absorbing: _isRecording ||
+              _isAsking, // Disable interaction when recording or asking
+          child: MotionTabBar(
+            tabBarColor: const Color(0xFF160239),
+            labels: const ["Home", "Record", "Recall"],
+            initialSelectedTab: "Home",
+            tabIconColor: const Color.fromARGB(255, 219, 197, 255),
+            tabSelectedColor: const Color(0xFFC75DEE),
+            textStyle: GoogleFonts.robotoMono(
+              textStyle: const TextStyle(
+                color: Color.fromARGB(255, 243, 236, 236),
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            icons: const [Icons.home, Icons.mic, Icons.question_answer],
+            onTabItemSelected: (int index) {
+              if (_isRecording || _isAsking) {
+                // Prevent changing tabs while recording or asking
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Please stop recording or asking before navigating.',
+                      style: GoogleFonts.robotoMono(),
+                    ),
+                  ),
+                );
+                return;
+              }
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
